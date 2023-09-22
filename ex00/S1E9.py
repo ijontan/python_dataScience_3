046834876aa5bfa57e1398eca29dddc5a55d0ed0
ijon <itan@student.42kl.edu.my>
2023-09-22 14:52:17 +0800

    update charater class

--- ex00/S1E9.py
+++ ex00/S1E9.py
@@ -3,23 +3,23 @@ from abc import ABC, abstractmethod
 
 class Character(ABC):
     """Abstract class for characters"""
+    first_name = None
+    is_alive = True
 
     @abstractmethod
     def __init__(self):
         """Constructor"""
         pass
 
+    def die(self):
+        """Kill the character"""
+        self.is_alive = False
+
 
 class Stark(Character):
     """Stark class"""
-    first_name = None
-    is_alive = True
 
     def __init__(self, first_name=None, is_alive=True):
         """Constructor"""
         self.first_name = first_name
         self.is_alive = is_alive
-
-    def die(self):
-        """Kill the character"""
-        self.is_alive = False
